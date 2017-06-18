80013fe4c14a495375b7e3576cb572b24cf0f96d
Chaitanya Khanna <c2khanna@uwaterloo.ca>
2017-06-18 16:39:14 -0400

    create basic UI

--- app/src/main/java/com/uwaterloo/proxtimeity/ChooseTypeActivity.java
+++ app/src/main/java/com/uwaterloo/proxtimeity/ChooseTypeActivity.java
@@ -1,7 +1,9 @@
 package com.uwaterloo.proxtimeity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseTypeActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class ChooseTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_type);
+    }
+
+    public void goToTimeActivity(View view) {
+        Intent goToTimeActivity = new Intent(ChooseTypeActivity.this, CreateTimeActivity.class);
+        startActivity(goToTimeActivity);
+    }
+
+    public void goToLocationActivity(View view) {
+        Intent goToLocationActivity = new Intent(ChooseTypeActivity.this, CreateLocationActivity.class);
+        startActivity(goToLocationActivity);
     }
 }
