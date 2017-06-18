cb16e10a4fb9565c5e01b8ebe3addb6181e405cc
Andrew Chung <andrewchungg@gmail.com>
2017-06-18 18:23:13 -0400

    added location reminder ui

--- app/src/main/java/com/uwaterloo/proxtimeity/CreateLocationActivity.java
+++ app/src/main/java/com/uwaterloo/proxtimeity/CreateLocationActivity.java
@@ -2,6 +2,8 @@ package com.uwaterloo.proxtimeity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
 
 public class CreateLocationActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CreateLocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_location);
+        Spinner locationSpinner = (Spinner) findViewById(R.id.locations_spinner);
+    }
+
+    public void saveLocationReminder(View view) {
+        return;
     }
 }
