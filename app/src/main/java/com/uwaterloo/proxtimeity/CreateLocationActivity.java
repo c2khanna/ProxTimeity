710a93f52d53cb2eb2b079adc8782d45076a3e9a
Naima Khan <naimak_95@yahoo.ca>
2017-06-19 17:53:51 -0400

    implement date and time picker modal on create location reminder screen

--- app/src/main/java/com/uwaterloo/proxtimeity/CreateLocationActivity.java
+++ app/src/main/java/com/uwaterloo/proxtimeity/CreateLocationActivity.java
@@ -1,11 +1,23 @@
 package com.uwaterloo.proxtimeity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class CreateLocationActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CreateLocationActivity extends AppCompatActivity
+        implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,61 @@ public class CreateLocationActivity extends AppCompatActivity {
 
     public void saveLocationReminder(View view) {
         return;
+    }
+
+    public void datePicker(View view){
+        CreateTimeActivity.DatePickerFragment fragment = new CreateTimeActivity.DatePickerFragment();
+        fragment.show(getSupportFragmentManager(),"date");
+    }
+
+    public void timePicker(View view) {
+        CreateTimeActivity.TimePickerFragment newFragment = new CreateTimeActivity.TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "time");
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar cal = new GregorianCalendar(year, month, day);
+        final java.text.DateFormat dateFormat = DateFormat.getLongDateFormat(getApplicationContext());
+        ((TextView) findViewById(R.id.dateSelected))
+                .setText(dateFormat.format(cal.getTime()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar cal = new GregorianCalendar(1, 1, 1, hourOfDay, minute);
+        String template = "hh:mm aaa";
+        ((TextView) findViewById(R.id.timeSelected))
+                .setText(DateFormat.format(template, cal.getTime()));
+    }
+
+    public static class DatePickerFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(),
+                    (DatePickerDialog.OnDateSetListener)
+                            getActivity(), year, month, day);
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener)
+                    getActivity(), hour, minute,
+                    false);
+        }
     }
 }
