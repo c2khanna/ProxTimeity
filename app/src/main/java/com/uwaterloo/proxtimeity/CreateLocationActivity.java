7f38809288208a3c4e6fa122d5c9ac141b6a9f7d
Andrew Chung <andrewchungg@gmail.com>
2017-07-13 01:30:39 -0400

    added basic geofence creation with hardcoded values

--- app/src/main/java/com/uwaterloo/proxtimeity/CreateLocationActivity.java
+++ app/src/main/java/com/uwaterloo/proxtimeity/CreateLocationActivity.java
@@ -12,6 +12,8 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
+import android.provider.SyncStateContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
@@ -29,12 +31,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -43,6 +49,8 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.UUID;
 
 public class CreateLocationActivity extends AppCompatActivity
         implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
@@ -50,10 +58,15 @@ public class CreateLocationActivity extends AppCompatActivity
     Calendar reminderExpiryDateTime = new GregorianCalendar();
     SharedPreferences mPrefs;
     private FusedLocationProviderClient mFusedLocationClient;
+
+    private PendingIntent mGeofencePendingIntent;
+    private ArrayList<Geofence> mGeofenceList = new ArrayList<>();
+    private GeofencingClient mGeofencingClient;
+    LatLng GeofenceLatLng1, GeofenceLatLng2;
+
     LatLng locationLatLng1, locationLatLng2;
     PlaceAutocompleteFragment autocompleteFragment;
     Place selectedPlace;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -148,6 +161,26 @@ public class CreateLocationActivity extends AppCompatActivity
                 .setText(DateFormat.format(template, reminderExpiryDateTime.getTime()));
     }
 
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        mGeofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
+    }
+
     public void saveLocationReminder(View view) {
         EditText edit = (EditText)findViewById(R.id.reminder_description);
         String description = edit.getText().toString();
@@ -190,6 +223,73 @@ public class CreateLocationActivity extends AppCompatActivity
         } else {
             Toast.makeText(this.getApplicationContext(), "Alarm is null", Toast.LENGTH_SHORT).show();
         }
+
+        mGeofencingClient = LocationServices.getGeofencingClient(this);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        boolean locationTracking = mPrefs.getBoolean("LocationTracking", false);
+
+        if (!locationTracking) {
+            prefsEditor.putBoolean("LocationTracking", true);
+            UUID idOne = UUID.randomUUID();
+
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            Log.i("SUCCES!!: ", "Location found!");
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                GeofenceLatLng1 = new LatLng(location.getLatitude(), location.getLongitude());
+                            }
+                        }
+                    });
+
+
+            mGeofenceList.add(new Geofence.Builder()
+                    // Set the request ID of the geofence. This is a string to identify this
+                    // geofence.
+                    .setRequestId(idOne.toString())
+                    .setCircularRegion(
+                            GeofenceLatLng1 == null ? 37 : GeofenceLatLng1.latitude,
+                            GeofenceLatLng1 == null ? -122 : GeofenceLatLng1.longitude,
+                            2000
+                    )
+                    .setExpirationDuration(60*60*1000)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build());
+
+        }
+
+        mGeofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Geofences added
+                        Log.i("SUCCESS: ", "Geofence added");
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to add geofences
+                        Log.i("FAILURE: ", "Geofence FAILED TO ADD");
+                    }
+                });
+
+
 
         //return to Home screen
         Intent goToMainScreen = new Intent(this, MainActivity.class);
---
+++ app/src/main/java/com/uwaterloo/proxtimeity/GeofenceTransitionsIntentService.java
@@ -0,0 +1,68 @@
+package com.uwaterloo.proxtimeity;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by andrewchung on 2017-07-12.
+ */
+
+public class GeofenceTransitionsIntentService extends IntentService {
+    private static final String TAG = GeofenceTransitionsIntentService.class.getSimpleName();
+
+    public GeofenceTransitionsIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            String errorMessage = Integer.toString(geofencingEvent.getErrorCode());
+            Log.e(TAG, errorMessage);
+            return;
+        }
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            // Get the transition details as a String.
+            Log.i(TAG, (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) ? "Geofence Entered for: " : "Geofence Exited for: ");
+            for (Geofence g : triggeringGeofences) {
+                Log.i(TAG, g.getRequestId());
+
+            }
+//            String geofenceTransitionDetails = getGeofenceTransitionDetails(
+//                    this,
+//                    geofenceTransition,
+//                    triggeringGeofences
+//            );
+
+            // Send notification and log the transition details.
+//            sendNotification(geofenceTransitionDetails);
+//            Log.i(TAG, geofenceTransitionDetails);
+
+        } else {
+            // Log the error.
+//            Log.e(TAG, getString(R.string.geofence_transition_invalid_type,
+//                    geofenceTransition));
+            Log.e(TAG, "GEOFENCE ERROR!");
+        }
+    }
+}
